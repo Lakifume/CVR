defa6bca43c325ce9c265ab7ff69853765a02453
Lakifume <lakifume.winnipeg@gmail.com>
2025-03-13 21:13:22 -0500

    13/03/2025

--- Plugins/SpriteTools/Source/SpriteTools/Private/SpriteToolsFunctionLibrary.cpp
+++ Plugins/SpriteTools/Source/SpriteTools/Private/SpriteToolsFunctionLibrary.cpp
@@ -7,9 +7,9 @@
 void USpriteToolsFunctionLibrary::OffsetPivot(UPaperSprite* SourceSprite, FVector2D PivotOffset)
 {
 	// reset to center
-	SourceSprite->SetPivotMode(ESpritePivotMode::Center_Center, FVector2D(), true);
+	//SourceSprite->SetPivotMode(ESpritePivotMode::Center_Center, FVector2D(), true);
 	
 	// offset it
-	const FVector2D PivotPosition = SourceSprite->GetPivotPosition();
-	SourceSprite->SetPivotMode(ESpritePivotMode::Custom, PivotPosition + PivotOffset, true);
+	//const FVector2D PivotPosition = SourceSprite->GetPivotPosition();
+	//SourceSprite->SetPivotMode(ESpritePivotMode::Custom, PivotPosition + PivotOffset, true);
 }
